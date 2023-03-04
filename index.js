c1da2ca569643282f650faae2f761d69ee5b52e3
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-03-04 11:45:32 -0600

    working on clock hand rotation

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const hourHand = document.querySelector('.hour-hand');
 function setTime() {
 const now = new Date()
 const seconds = now.getSeconds();
+const secondRotation = 
 console.log(seconds)
+
+const mins = min.getMinuets();
+const minRotation = 
+
+const hour = now.getHours();
+const hourRotation = 
 }
 
 setInterval(setTime, 1000)
