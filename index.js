9bc55713e7e4ff8a3a71205bed733877a6adfa90
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-03-05 16:03:44 -0600

    clock now functions, new css hand styles

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setTime() {
 const now = new Date()
-const seconds = now.getSeconds();
-const secondRotation = 
-console.log(seconds)
 
-const mins = min.getMinuets();
-const minRotation = 
+const seconds = now.getSeconds();
+const secondRotation = ((seconds/ 60) * 360) + 90;
+secondHand.style.transform = `rotate(${secondRotation}deg)`;
+
+const mins = now.getMinutes();
+const minRotation = ((mins/ 60 ) * 360) + ((seconds/60)*6) + 90;
+minHand.style.transform = `rotate(${minRotation}deg)`;
 
 const hour = now.getHours();
-const hourRotation = 
+const hourRotation = ((hour/ 12)* 360) + ((mins/60)*30) + 90;
+hourHand.style.transform = `rotate(${hourRotation}deg)`;
 }
 
-setInterval(setTime, 1000)
+setInterval(setTime, 1000);
+setTime();
